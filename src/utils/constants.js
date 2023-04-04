096f31265f2dc4aa0fa734e4a21fa2e951262d32
hrithin220900 <65213514+hrithin220900@users.noreply.github.com>
2023-04-04 17:46:16 +0530

    master

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const SERVER = "http://localhost:8800/api" 
+export const SERVER = "https://virtualacharya-server.vercel.app/api" 
 
 export const subjects = [
     "math", "physics", "dbms", "Machine Learning", "FLAT", "Compiler Design"

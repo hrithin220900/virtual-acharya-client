af12575ee0534aa529142985cb7cf23f6aa17d9c
hrithin220900 <65213514+hrithin220900@users.noreply.github.com>
2023-04-07 14:22:06 +0530

    topbar changes

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -9,14 +9,16 @@ const Topbar = () => {
 
   return (
     <div className='flex items-center justify-between bg-[#212121] fixed top-0 left-0 px-10 py-5 shadow-md w-screen h-[80px]'>
-        <div className='text-xl font-semibold'>
-            Virtual Aacharya
-        </div>
+        <Link className='link hover:text-orange-500'>
+          <div className='text-xl font-semibold'>
+              Virtual Aacharya
+          </div>
+        </Link>
         <div className='flex items-center gap-5'>
           {isUserPresent ? (
             <div className='flex items-center gap-5'>
               <span className='text-white font-bold text-lg'>{user?.name}</span>
-              <button className='hover:text-orange-400 cursor-pointer' onClick={() => {navigate("/login"); localStorage.clear(); window.location.reload()}} >Logout</button>
+              <button className='hover:text-orange-400 cursor-pointer border-2 border-white' onClick={() => {navigate("/login"); localStorage.clear(); window.location.reload()}} >Logout</button>
             </div>
           ) : <>
             <Link className='link' to="/register"><span className='hover:text-orange-400 cursor-pointer'>Register</span></Link>
